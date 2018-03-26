e29e4c295f643e65cbcc8712727fda1b967159cd
Kyle Tenzeldam and Ross Fletcher <ross@>
2018-03-26 12:59:25 -0700

    add firebase functionality

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantsListActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantsListActivity.java
@@ -1,12 +1,15 @@
 package com.epicodus.myrestaurants.ui;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.adapters.RestaurantListAdapter;
 import com.epicodus.myrestaurants.models.Restaurant;
@@ -26,6 +29,8 @@ public class RestaurantsListActivity extends AppCompatActivity {
     public static final String TAG = RestaurantsListActivity.class.getSimpleName();
     @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
     private RestaurantListAdapter mAdapter;
+    private SharedPreferences mSharedPreferences;
+    private String mRecentAddress;
 
 
 
@@ -36,14 +41,20 @@ public class RestaurantsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-
         ButterKnife.bind(this);
+
 
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
 
         getRestaurants(location);
+
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_LOCATION_KEY, null);
+//        if (mRecentAddress != null) {
+//            getRestaurants(mRecentAddress);
+//        }
     }
 
     private void getRestaurants(String location) {
